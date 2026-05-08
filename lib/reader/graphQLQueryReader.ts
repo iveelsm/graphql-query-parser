1889cfba5820a0c5af3b34d958560f7abbdab3c7
Mikey Sleevi <development@frogman.simplelogin.com>
2026-05-08 08:41:13 -0600

    update imports

--- lib/reader/graphQLQueryReader.ts
+++ lib/reader/graphQLQueryReader.ts
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+import * as fs from "node:fs";
 
 import DirectoryReader from "./directory/directoryReader.ts";
 import UnsupportedTypeError from "./errors/unsupportedTypeError.ts";
